9c65f5f6ada36f01de005aa05642fccd0ea5bd68
AllenChen-12 <allenc2028@student.cis.edu.hk>
2025-12-01 12:00:25 +0800

    final ()

--- src/Cube2.java
+++ src/Cube2.java
@@ -16,14 +16,63 @@ public class Cube2 {
         return basicCube.getSide();
     }
 
+    public String getColor() {
+        return color;
+    }
+    
+    public Cube2(String color) {
+        this.color = color;
+    }
+    
+    public Cube2(int side, String color){
+        basicCube = new Cube(side);
+        this.color = color;
+    }
+
     //the following methods have been started for you, but need to be completed
-    public int calculateVolume() { return 0;}
-    public int calculateSurfaceArea() { return 0;}
-    public Cube2 add(Cube2 otherCube) { return new Cube2(); }
-    public Cube2 minus(Cube2 otherCube) { return new Cube2(); }
-    public boolean equals(Cube2 otherCube) { return false; }
+    public int calculateVolume() { 
+        return basicCube.calculateVolume();
+    }
+    
+    public int calculateSurfaceArea() { 
+        return basicCube.calculateSurfaceArea();
+    }
+    
+    public Cube2 add(Cube2 otherCube) { 
+        int x = this.getSide();
+        int y = otherCube.getSide();
+        int z = (int) Math.sqrt(x*x + y*y);
+
+        if (z*z != x*x + y*y){
+            throw new IllegalArgumentException();
+        }
+
+        return new Cube2(z, this.color); 
+    }
+
+    public Cube2 minus(Cube2 otherCube) { 
+        int x = this.getSide();
+        int y = otherCube.getSide();
+        int z = (int) Math.sqrt(x*x - y*y);
+
+         if (z*z != x*x - y*y){
+            throw new IllegalArgumentException();
+        }
+
+        return new Cube2(z, this.color); 
+    }
+    
+    public boolean equals(Cube2 otherCube) { 
+        if (this.getSide() != otherCube.getSide()){
+            return false;
+        }
+        if (!this.color.equals(otherCube.color)){
+            return false;
+        }
+        return true;
+     }
 
     public String toString() {
-        return basicCube.toString();
+        return "Cube{side=" + getSide() + ", color=" + '"' + color + '"' + "}";
     }
 }
